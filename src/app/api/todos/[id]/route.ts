fd450081ed2600fb842a75655e7f7c885235aa2a
AngibaultMaxime <maxime.angibault@gmail.com>
2026-01-23 17:39:46 -0500

    Correction du patch (qui mettait tous les attributs non modifiés a undefined) + DELETE + usage de parseIdParam()

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,6 @@
 import { requireAdmin, requireAuth } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
+import { parseIdParam } from "@/lib/utils";
 import { updateTodoSchema } from "@/schemas/todo.schema";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -15,13 +16,8 @@ export async function GET(
     const { id } = await params;
 
     // Vérification du format de l'ID
-    const todoId = parseInt(id, 10);
-    if (isNaN(todoId)) {
-      return NextResponse.json(
-        { error: "Format d'ID invalide. Doit être un nombre entier." },
-        { status: 400 },
-      );
-    }
+    const todoId = parseIdParam(id);
+    if (todoId instanceof NextResponse) return todoId;   // Retourne todoId qui est un NextResponse en cas d'erreur
 
     // Récupérer le todo avec les relations
     const todo = await prisma.todo.findUnique({
@@ -69,14 +65,9 @@ export async function PATCH(
     // Récupérer l'ID du todo depuis les paramètres
     const { id } = await params;
 
-    // Verification du format de l'ID
-    const todoId = parseInt(id, 10);
-    if (isNaN(todoId)) {
-      return NextResponse.json(
-        { error: "Format d'ID invalide. Doit être un nombre entier." },
-        { status: 400 },
-      );
-    }
+    // Vérification du format de l'ID
+    const todoId = parseIdParam(id);
+    if (todoId instanceof NextResponse) return todoId;   // Retourne todoId qui est un NextResponse en cas d'erreur
 
     // Validation des données avec Zod
     const body = await req.json();
@@ -94,13 +85,29 @@ export async function PATCH(
     const updateTodo = await prisma.todo.update({
       where: { id: todoId },
       data: {
-        title: validatedData.title,
-        description: validatedData.description,
-        status: validatedData.status,
-        priority: validatedData.priority,
-        dueDate: validatedData.dueDate ? new Date(validatedData.dueDate) : null, // Conversion String -> Date
-        categoryId: validatedData.categoryId ?? null,
-        assignedToId: validatedData.assignedToId ?? null,
+        ...(validatedData.title !== undefined && {
+          title: validatedData.title,
+        }),
+        ...(validatedData.description !== undefined && {
+          description: validatedData.description,
+        }),
+        ...(validatedData.status !== undefined && {
+          status: validatedData.status,
+        }),
+        ...(validatedData.priority !== undefined && {
+          priority: validatedData.priority,
+        }),
+        ...(validatedData.dueDate !== undefined && {
+          dueDate: validatedData.dueDate
+            ? new Date(validatedData.dueDate)
+            : null,
+        }),
+        ...(validatedData.categoryId !== undefined && {
+          categoryId: validatedData.categoryId,
+        }),
+        ...(validatedData.assignedToId !== undefined && {
+          assignedToId: validatedData.assignedToId,
+        }),
       },
       include: {
         category: true,
@@ -139,6 +146,58 @@ export async function PATCH(
     console.error("Update todo error:", error);
     return NextResponse.json(
       { error: "Erreur lors de la mise à jour du todo" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    // Seul l'admin peut supprimer des todos
+    const user = requireAdmin(req);
+
+    // Récupérer l'ID du todo depuis les paramètres
+    const { id } = await params;
+
+    // Vérification du format de l'ID
+    const todoId = parseIdParam(id);
+    if (todoId instanceof NextResponse) return todoId;   // Retourne todoId qui est un NextResponse en cas d'erreur
+
+    // Verifier que le todo existe
+    const existingTodo = await prisma.todo.findUnique({
+      where: { id: todoId },
+    });
+    if (!existingTodo) {
+      return NextResponse.json({ error: "Todo non trouvé" }, { status: 404 });
+    }
+
+    // Supprimer le todo
+    await prisma.todo.delete({
+      where: { id: todoId },
+    });
+
+    return NextResponse.json({ message: "Todo supprimé avec succès" });
+  } catch (error: any) {
+    if (error.message === "UNAUTHORIZED") {
+      return NextResponse.json(
+        { error: "Utilisateur non authentifié" },
+        { status: 401 },
+      );
+    }
+
+    if (error.message === "FORBIDDEN") {
+      return NextResponse.json(
+        { error: "Accès refusé. Réservé aux administrateurs." },
+        { status: 403 },
+      );
+    }
+
+    console.error("Delete todo error:", error);
+    return NextResponse.json(
+      { error: "Erreur lors de la suppression du todo" },
       { status: 500 },
     );
   }
