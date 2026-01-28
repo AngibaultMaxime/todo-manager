d8adf8317983f8bf0851eeedf5777382e8cd56d8
AngibaultMaxime <maxime.angibault@gmail.com>
2026-01-28 15:55:03 -0500

    Ajout commentaire header

--- src/app/(dashboard)/todos/page.tsx
+++ src/app/(dashboard)/todos/page.tsx
@@ -200,6 +200,7 @@ export default function TodosPage() {
 
   return (
     <ProtectedRoute>
+      {/* Header */}
       <div className="min-h-screen bg-gray-100">
         <header className="bg-white shadow-md border-b border-gray-200">
           <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
