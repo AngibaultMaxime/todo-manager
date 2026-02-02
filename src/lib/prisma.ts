ed4b27cf30c1aafd0ca2cdec1cc26ad3b8a62401
AngibaultMaxime <maxime.angibault@gmail.com>
2026-02-02 15:51:59 -0500

    Fix Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,16 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// Assure-toi que Prisma prend l'URL de Vercel
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL, // obligatoire en production
+      },
+    },
+  })
 
+// Reuse Prisma client in dev to avoid hot reload issues
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
